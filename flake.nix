e4425d36ec644e8d2756093191eb6c87d0eac670
hugosenari <hugosenari@gmail.com>
2022-05-12 11:59:01 -0300

    chore: re index xevloop

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."xevloop-master".dir   = "master";
+  inputs."xevloop-master".owner = "nim-nix-pkgs";
+  inputs."xevloop-master".ref   = "master";
+  inputs."xevloop-master".repo  = "xevloop";
+  inputs."xevloop-master".type  = "github";
+  inputs."xevloop-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."xevloop-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
